8fa7d62a10293513ffe8c49ed2ab6a9517d78f5d
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-11-18 16:16:00 +0330

    created navbar component

---
+++ src/app/ui/dashboard/navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+// "use client";
+
+// import { usePathname } from "next/navigation";
+
+// const Navbar = () => {
+//   const pathname = usePathname();
+//   return <div className="">{pathname.split("/").pop()}</div>;
+// };
+
+// export default Navbar;
