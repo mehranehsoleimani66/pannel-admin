909e1f5506a62e349f4e98c71bee83c02352b2d0
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-11-18 18:08:41 +0330

    modified navbar component

--- src/app/ui/dashboard/navbar/Navbar.jsx
+++ src/app/ui/dashboard/navbar/Navbar.jsx
@@ -1,10 +1,36 @@
-// "use client";
+"use client";
 
-// import { usePathname } from "next/navigation";
+import { usePathname } from "next/navigation";
+import {
+  MdNotifications,
+  MdOutlineChat,
+  MdPublic,
+  MdSearch
+} from "react-icons/md";
 
-// const Navbar = () => {
-//   const pathname = usePathname();
-//   return <div className="">{pathname.split("/").pop()}</div>;
-// };
+const Navbar = () => {
+  const pathname = usePathname();
+  return (
+    <div className="flex justify-between p-3 m-5 rounded-xl items-center bg-[#182237]">
+      <div className="font-bold text-[#b7bac1] capitalize">
+        {pathname.split("/").pop()}
+      </div>
+      <div className="flex items-center justify-between gap-3 p-3 rounded-xl">
+        <div className="flex justify-center items-center gap-1 pl-1 bg-[#2e374a] rounded-lg">
+          <MdSearch />
+          <input
+            placeholder="search..."
+            className="bg-transparent text-[white]"
+          />
+        </div>
+        <div className="flex gap-5">
+          <MdOutlineChat size={20} />
+          <MdNotifications size={20} />
+          <MdPublic size={20} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-// export default Navbar;
+export default Navbar;
