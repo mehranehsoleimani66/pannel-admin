d5351f70a16186f6b6aec1ed06db9f8e8ca21de4
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-11-18 16:16:15 +0330

    created sidebar component

---
+++ src/app/ui/dashboard/sidebar/Sidebar.jsx
@@ -0,0 +1,112 @@
+import Image from "next/image";
+import {
+  MdDashboard,
+  MdSupervisedUserCircle,
+  MdShoppingBag,
+  MdAttachMoney,
+  MdWork,
+  MdAnalytics,
+  MdPeople,
+  MdOutlineSettings,
+  MdHelpCenter,
+  MdLogout
+} from "react-icons/md";
+import MenuLink from "./menulink/MenuLink";
+
+const Sidebar = () => {
+  const menuItems = [
+    {
+      title: "Pages",
+      list: [
+        {
+          title: "Dashboard",
+          path: "/dashboard",
+          icon: <MdDashboard />
+        },
+        {
+          title: "Users",
+          path: "/dashboard/users",
+          icon: <MdSupervisedUserCircle />
+        },
+        {
+          title: "Products",
+          path: "/dashboard/products",
+          icon: <MdShoppingBag />
+        },
+        {
+          title: "Transactions",
+          path: "/dashboard/transactions",
+          icon: <MdAttachMoney />
+        }
+      ]
+    },
+    {
+      title: "Analytics",
+      list: [
+        {
+          title: "Revenue",
+          path: "/dashboard/revenue",
+          icon: <MdWork />
+        },
+        {
+          title: "Reports",
+          path: "/dashboard/reports",
+          icon: <MdAnalytics />
+        },
+        {
+          title: "Teams",
+          path: "/dashboard/teams",
+          icon: <MdPeople />
+        }
+      ]
+    },
+    {
+      title: "User",
+      list: [
+        {
+          title: "Settings",
+          path: "/dashboard/settings",
+          icon: <MdOutlineSettings />
+        },
+        {
+          title: "Help",
+          path: "/dashboard/help",
+          icon: <MdHelpCenter />
+        }
+      ]
+    }
+  ];
+
+  return (
+    <div className="sticky top-10 w-52">
+      <div className="flex items-center gap-5 mb-5">
+        <Image
+          src="/noavatar.png"
+          alt=""
+          width={50}
+          height={50}
+          className="rounded-full object-cover"
+        />
+        <div className="flex flex-col ">
+          <span className="font-medium">joe dev</span>
+          <span className="text-sm text-[#b7bac1]">administratore</span>
+        </div>
+      </div>
+      <ul className="list-none">
+        {menuItems.map((cat) => (
+          <li
+            className="text-[#b7bac1] font-bold text-xs mt-3 mb-3 "
+            key={cat.title}
+          >
+            <span>{cat.title}</span>
+            {cat.list.map((item) => (
+              <MenuLink item={item} key={item.title} />
+            ))}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
