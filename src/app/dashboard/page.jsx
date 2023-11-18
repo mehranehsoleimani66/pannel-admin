7a59ee0f2266419269cbd6f3a1f9659df2dae62f
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-11-18 18:06:35 +0330

    added components in this components

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import Card from "../ui/dashboard/card/Card";
+import RightBar from "../ui/dashboard/rightbar/RightBar";
+import Transection from "../ui/dashboard/transection/Transection";
+import Chart from "../ui/dashboard/chart/Chart";
 
 const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  return (
+    <div className="flex justify-between  gap-5">
+      <div className="flex-3 flex flex-col gap-5">
+        <div className="flex gap-3 justify-between  items-center  ">
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <Chart />
+        <Transection />
+      </div>
+
+      <div className="flex flex-1">
+        <RightBar />
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
