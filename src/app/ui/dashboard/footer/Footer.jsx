5f28c0c08e44f2887a007beb74093248e9d8236c
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-11-19 22:19:08 +0330

    created footer component

---
+++ src/app/ui/dashboard/footer/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <div className="flex  justify-between items-center mt-5 text-[#b7bac1]">
+      <div className="font-bold">Lama Dev</div>
+      <div className="text-xs">© All rights reserved.</div>
+    </div>
+  );
+};
+
+export default Footer;
