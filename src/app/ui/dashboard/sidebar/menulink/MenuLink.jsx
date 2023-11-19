4060fdd384a94a9aca54e073cbfe83b28016f012
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-11-19 19:37:51 +0330

    modified

--- src/app/ui/dashboard/sidebar/menulink/MenuLink.jsx
+++ src/app/ui/dashboard/sidebar/menulink/MenuLink.jsx
@@ -6,17 +6,15 @@ const MenuLink = ({ item }) => {
   const pathname = usePathname();
   console.log(pathname);
   return (
-    <div>
-      <Link
-        href={item.path}
-        className={`flex  p-5 items-center gap-3  rounded-xl ${
-          pathname === item.path ? "bg-[#2e374a]" : ""
-        } hover:bg-[#2e374a]`}
-      >
-        {item.icon}
-        {item.title}
-      </Link>
-    </div>
+    <Link
+      href={item.path}
+      className={`flex  p-5 items-center gap-3  rounded-xl ${
+        pathname === item.path ? "bg-[#2e374a]" : ""
+      } hover:bg-[#2e374a]`}
+    >
+      {item.icon}
+      {item.title}
+    </Link>
   );
 };
 
