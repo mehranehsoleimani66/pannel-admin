721033aafb615e175fbee3adbb34d3ad8bacb923
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-11-19 19:36:57 +0330

    modified

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -6,7 +6,7 @@ import Transaction from "../ui/dashboard/Transaction/Transaction";
 
 const DashboardPage = () => {
   return (
-    <div className="flex justify-between  gap-3">
+    <div className="flex justify-between  gap-3 ml-3">
       <div className="flex-3 flex flex-col gap-5 ">
         <div className="flex w-full justify-center gap-3  ml-3">
           <Card />
