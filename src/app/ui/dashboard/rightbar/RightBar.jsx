e1aae7cdaf05407157b9335d42812609824d1374
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-11-19 19:37:34 +0330

    modified

--- src/app/ui/dashboard/rightbar/RightBar.jsx
+++ src/app/ui/dashboard/rightbar/RightBar.jsx
@@ -4,7 +4,7 @@ import { MdPlayCircleFilled, MdReadMore } from "react-icons/md";
 const RightBar = () => {
   return (
     <div className="fixed">
-      <div className="relative rounded-xl pt-5  pb-5 pl-5 pr-5  mb-5 bg-gradient-to-t from-[#182237] to-[#253352] ">
+      <div className=" relative flex flex-col  gap-5 rounded-xl pt-5  pb-5 pl-5 pr-5  mb-5 bg-gradient-to-t from-[#182237] to-[#253352] ">
         <div className="absolute bottom-0 right-0 w-1/2 h-1/2">
           <Image
             src="/astronaut.png"
@@ -13,13 +13,13 @@ const RightBar = () => {
             className="object-contain opacity-20"
           />
         </div>
-        <div className="flex flex-col gap-2 ">
+        <div className="flex flex-col gap-3 ">
           <span className="font-bold">🔥 Available Now</span>
           <h3> How to use the new version of the admin dashboard?</h3>
           <span className="text-xs font-medium text-[#b7bac1]">
             Takes 4 minutes to learn
           </span>
-          <p className="text-sm font-medium text-[#b7bac1]">
+          <p className="text-xs font-medium text-[#b7bac1]">
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
             Reprehenderit eius libero perspiciatis recusandae possimus.
           </p>
@@ -31,7 +31,7 @@ const RightBar = () => {
       </div>
 
       <div className="relative rounded-xl pt-5 pb-5 mb-5 bg-gradient-to-t from-[#182237] to-[#253352] ">
-        <div className="flex flex-col gap-2  pt-5  pb-5 pl-5 pr-5">
+        <div className="flex flex-col gap-2 pt-5  pb-5 pl-5 pr-5">
           <span className='"font-bold"'>🚀 Coming Soon</span>
           <h3 className=" font-medium text-white">
             New server actions are available, partial pre-rendering is coming
@@ -40,7 +40,7 @@ const RightBar = () => {
           <span className="text-xs text-[#b7bac1] ">
             Boost your productivity
           </span>
-          <p className="text-sm text-[#b7bac1] ">
+          <p className="text-xs text-[#b7bac1] ">
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
             Reprehenderit eius libero perspiciatis recusandae possimus.
           </p>
