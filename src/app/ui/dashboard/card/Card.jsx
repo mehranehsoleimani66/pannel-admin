3dddfa0a6fb79c77b33d9c98b4d961e744ce3232
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-11-19 15:52:17 +0330

    added chart component

--- src/app/ui/dashboard/card/Card.jsx
+++ src/app/ui/dashboard/card/Card.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { MdSupervisedUserCircle } from "react-icons/md";
 
 const Card = () => {
-  return <div>Card</div>;
+  return (
+    <div className="flex bg-[#182237] w-full rounded-md  p-3">
+      <MdSupervisedUserCircle size={24} />
+      <div className="flex flex-col gap-5">
+        <span>Total User</span>
+        <span className="text-2xl font-bold">10.234</span>
+        <span className="text-xs font-medium">
+          <span className="text-lime-500">120%</span> more than previous week
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
