eb590cf77a21819aa7d6c1443625bf984302ce7f
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-11-19 22:18:15 +0330

    added footer component

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -1,14 +1,17 @@
+import Footer from "../ui/dashboard/footer/Footer";
 import Navbar from "../ui/dashboard/navbar/Navbar";
 import Sidebar from "../ui/dashboard/sidebar/Sidebar";
+
 const Layout = ({ children }) => {
   return (
     <div className="flex">
-      <div className=" flex-0.5   bg-[#182237] pl-5">
+      <div className="flex-0.5   bg-[#182237] pl-5">
         <Sidebar />
       </div>
       <div className=" flex-3 flex-col">
         <Navbar />
         {children}
+        <Footer />
       </div>
     </div>
   );
