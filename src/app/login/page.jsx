384bbe85e372731a58318f3ac3a7415b5a76d60b
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-11-21 21:39:54 +0330

    added login page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className="w-full h-[100vh] flex flex-col items-center justify-center">
+      <form
+        action=""
+        className="bg-[#182237] p-[50px] rounded-xl w-[500px] h-[500px] flex flex-col gap-[30px] justify-center"
+      >
+        <input
+          type="text"
+          placeholder="userName..."
+          className="p-[30px] border-2 border-solid border-[#2e374a] bg-[#151c2c] text-white"
+        />
+        <input
+          type="password"
+          placeholder="password..."
+          className="p-[30px] border-2 border-solid border-[#2e374a] bg-[#151c2c] text-white"
+        />
+        <button className="p-8 bg-teal-500 border-none rounded-md">
+          LogIn
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
