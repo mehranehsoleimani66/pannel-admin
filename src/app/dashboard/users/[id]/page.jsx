eef5299847d48d602b5356a6620317902cbf616e
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-11-21 21:39:37 +0330

    modified

--- src/app/dashboard/users/[id]/page.jsx
+++ src/app/dashboard/users/[id]/page.jsx
@@ -2,28 +2,61 @@ import Image from "next/image";
 
 const SingleUserPage = () => {
   return (
-    <div className="">
-      <div className="">
-        <div className="relative">
+    <div className="flex gap-[50px] mt-5">
+      <div className="flex-1 bg-[#182237] p-5 h-max rounded-xl font-bold text-[#b7bac1]">
+        <div className="relative w-full h-[300px] overflow-hidden rounded-xl mb-5">
           <Image src="/noavatar.png" alt="" fill />
         </div>
-        jojn deo
+        John Deo
       </div>
-      <div className="">
-        <form action="" className="">
-          <input type="hidden" name="id" value="" />
-          <label>Username</label>
-          <input type="text" name="username" placeholder="" />
-          <label>Email</label>
-          <input type="email" name="email" placeholder="" />
-          <label>Password</label>
-          <input type="password" name="password" />
-          <label>Phone</label>
-          <input type="text" name="phone" placeholder="" />
-          <label>Address</label>
-          <textarea type="text" name="address" placeholder="" />
-          <label>Is Admin?</label>
-          <select name="isAdmin" id="isAdmin">
+      <div className="flex-3 bg-[#182237] p-5 rounded-xl mr-5">
+        <form action="" className="flex flex-col">
+          <input
+            type="hidden"
+            name="id"
+            value=""
+            className="p-5 border border-solid border-[#2e374a] rounded bg-[#151c2c] text-white mt-[10px] mb-[10px]"
+          />
+          <label className="text-xs">Username</label>
+          <input
+            type="text"
+            name="username"
+            placeholder=""
+            className="p-5 border border-solid border-[#2e374a] rounded bg-[#151c2c] text-white mt-[10px] mb-[10px]"
+          />
+          <label className="text-xs">Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder=""
+            className="p-5 border border-solid border-[#2e374a] rounded bg-[#151c2c] text-white mt-[10px] mb-[10px]"
+          />
+          <label className="text-xs">Password</label>
+          <input
+            type="password"
+            name="password"
+            className="p-5 border border-solid border-[#2e374a] rounded bg-[#151c2c] text-white mt-[10px] mb-[10px]"
+          />
+          <label className="text-xs">Phone</label>
+          <input
+            type="text"
+            name="phone"
+            placeholder=""
+            className="p-5 border border-solid border-[#2e374a] rounded bg-[#151c2c] text-white mt-[10px] mb-[10px]"
+          />
+          <label className="text-xs">Address</label>
+          <textarea
+            type="text"
+            name="address"
+            placeholder=""
+            className="p-5 border border-solid border-[#2e374a] rounded bg-[#151c2c] text-white mt-[10px] mb-[10px]"
+          />
+          <label className="text-xs">Is Admin?</label>
+          <select
+            name="isAdmin"
+            id="isAdmin"
+            className="p-5 border border-solid border-[#2e374a] rounded bg-[#151c2c] text-white mt-[10px] mb-[10px]"
+          >
             <option value={true} selected="">
               Yes
             </option>
@@ -31,8 +64,12 @@ const SingleUserPage = () => {
               No
             </option>
           </select>
-          <label>Is Active?</label>
-          <select name="isActive" id="isActive">
+          <label className="text-xs">Is Active?</label>
+          <select
+            name="isActive"
+            id="isActive"
+            className="p-5 border border-solid border-[#2e374a] rounded bg-[#151c2c] text-white mt-[10px] mb-[10px]"
+          >
             <option value={true} selected="">
               Yes
             </option>
@@ -40,7 +77,9 @@ const SingleUserPage = () => {
               No
             </option>
           </select>
-          <button>Update</button>
+          <button className="w-full p-5 bg-teal-500 border-none rounded-md cursor-pointer mt-5 text-white">
+            Update
+          </button>
         </form>
       </div>
     </div>
