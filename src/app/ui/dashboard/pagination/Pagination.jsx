2977f80597d3f0157077246acb0de81e51f8fb3b
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-11-25 22:49:46 +0330

    added pagination

--- src/app/ui/dashboard/pagination/Pagination.jsx
+++ src/app/ui/dashboard/pagination/Pagination.jsx
@@ -1,15 +1,42 @@
-import React from "react";
+"use client";
 
-const Pagination = () => {
+import { Cursor } from "mongoose";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+const Pagination = ({ count }) => {
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+  const pathname = usePathname();
+
+  const page = searchParams.get("page") || 1;
+
+  const params = new URLSearchParams(searchParams);
+  const ITEM_PER_PAGE = 2;
+
+  const hasPrev = ITEM_PER_PAGE * (parseInt(page) - 1) > 0;
+  const hasNext = ITEM_PER_PAGE * (parseInt(page) - 1) + ITEM_PER_PAGE < count;
+
+  const handleChangePage = (type) => {
+    type === "prev"
+      ? params.set("page", parseInt(page) - 1)
+      : params.set("page", parseInt(page) + 1);
+    replace(`${pathname}?${params}`);
+  };
+
   return (
     <div className="flex justify-between items-center p-3">
       <button
-        disabled
-        className=" pr-[10px] pl-[10px] pt-[5px] pb-[5px] cursor-not-allowed bg-gray-500 text-black "
+        disabled={!hasPrev}
+        className=" cursor-pointer  disabled:cursor-not-allowed disabled:text-gray-500 "
+        onClick={() => handleChangePage("prev")}
       >
         Previus
       </button>
-      <button className="pr-[10px] pl-[10px] pt-[5px] pb-[5px] cursor-pointer bg-gray-200 text-black ">
+      <button
+        disabled={!hasNext}
+        className="  disabled:cursor-not-allowed disabled:text-gray-500"
+        onClick={() => handleChangePage("next")}
+      >
         next
       </button>
     </div>
