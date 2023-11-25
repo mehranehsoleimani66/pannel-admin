360cb9724ee649357a519c6e1d09f52c32970aef
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-11-25 16:52:41 +0330

    modified

--- src/app/dashboard/users/page.jsx
+++ src/app/dashboard/users/page.jsx
@@ -1,19 +1,24 @@
-import Search from "@/app/ui/dashboard/search/Search";
 import Link from "next/link";
 import Image from "next/image";
+import { fetchUsers } from "@/app/libs/data";
+import Search from "@/app/ui/dashboard/search/Search";
 import Pagination from "@/app/ui/dashboard/pagination/Pagination";
-const UserPage = () => {
+
+const UserPage = async ({ searchParams }) => {
+  const q = searchParams?.q || "";
+  const users = await fetchUsers(q);
+  console.log("mehrane", users);
+
   return (
     <div className="p-5 bg-[#182237] mt-5 ml-5 mr-5 rounded-xl">
       <div className="flex items-center justify-between">
-        <Search placeholder="search a user..." />
+        <Search placeholder="Search for a user..." />
         <Link href="/dashboard/users/add">
           <button className="p-2 text-[#b7bac1] bg-[#5d57c9] border-none rounded-md cursor-pointer">
             Add New
           </button>
         </Link>
       </div>
-
       <table className="w-full">
         <thead>
           <tr>
@@ -26,34 +31,36 @@ const UserPage = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
-              <div className="flex items-center  gap-5">
-                <Image
-                  src="/avatar.png"
-                  alt=""
-                  width={40}
-                  height={40}
-                  className="rounded-full object-cover"
-                />
-                Jojn Doe
-              </div>
-            </td>
-            <td>John@gmail.com</td>
-            <td>2023.08.06</td>
-            <td>Admin</td>
-            <td>Active</td>
-            <td className="flex gap-3">
-              <Link href="/dashboard/users/test">
-                <button className="bg-[teal] pt-2 pb-3 text-center pr-3 pl-3 rounded-md text-white border-none cursor-pointer">
-                  Veiw
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>
+                <div className="flex items-center  gap-5">
+                  <Image
+                    src={user.img || "/noavatar.png"}
+                    alt=""
+                    width={40}
+                    height={40}
+                    className="rounded-full "
+                  />
+                  {user.username}
+                </div>
+              </td>
+              <td>{user.email}</td>
+              <td>{user.createdAt?.toString().slice(4, 16)}</td>
+              <td>{user.isAdmin ? "Admin" : "Client"}</td>
+              <td>{user.isActive ? "active" : "passive"}</td>
+              <td className="flex gap-3">
+                <Link href={`/dashboard/users/${user.id}`}>
+                  <button className="bg-[teal] pt-2 pb-3 text-center pr-3 pl-3 rounded-md text-white border-none cursor-pointer">
+                    Veiw
+                  </button>
+                </Link>
+                <button className="bg-[crimson] pt-2 pb-2 pr-3 pl-3 text-center rounded-md text-white border-none cursor-pointer">
+                  Delet
                 </button>
-              </Link>
-              <button className="bg-[crimson] pt-2 pb-2 pr-3 pl-3 text-center rounded-md text-white border-none cursor-pointer">
-                Delet
-              </button>
-            </td>
-          </tr>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
       <Pagination />
