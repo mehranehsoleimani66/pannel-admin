cb9edf498a3cdc2de3b02076be6774152c85713f
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-11-26 13:05:37 +0330

    modified

--- src/app/ui/dashboard/search/Search.jsx
+++ src/app/ui/dashboard/search/Search.jsx
@@ -1,13 +1,33 @@
+"use client";
+
+import { usePathname, useSearchParams, useRouter } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 import React from "react";
 import { MdSearch } from "react-icons/md";
 
 const Search = ({ placeholder }) => {
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+  const pathname = usePathname();
+
+  const handleSearch = useDebouncedCallback((e) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", 1);
+    if (e.target.value) {
+      e.target.value.length >= 2 && params.set("q", e.target.value);
+    } else {
+      params.delete("q");
+    }
+    replace(`${pathname}?${params}`);
+  }, 300);
+
   return (
     <div className="flex justify-center items-center gap-1 pl-1 bg-[#2e374a] rounded-lg">
       <MdSearch />
       <input
         placeholder={placeholder}
         className="bg-transparent text-[white] p-1"
+        onChange={handleSearch}
       />
     </div>
   );
