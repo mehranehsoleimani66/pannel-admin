4e7043a193290ca7dd02dc4b2bea7962faf1064d
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-11-28 07:53:52 +0330

    fetch users,products,singleuser,singleproduct

--- src/app/libs/data.js
+++ src/app/libs/data.js
@@ -1,5 +1,5 @@
-import { connectToDB } from "./Utils";
 import { Product, User } from "./models";
+import { connectToDB } from "./utils";
 
 export const fetchUsers = async (q, page) => {
   const regex = new RegExp(q, "i");
@@ -47,5 +47,16 @@ export const fetchProducts = async (q, page) => {
   } catch (err) {
     console.log(err);
     throw new Error("Failed to fetch products!");
+  }
+};
+
+export const fetchProduct = async (id) => {
+  try {
+    connectToDB();
+    const product = await Product.findById(id);
+    return product;
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to fetch product!");
   }
 };
