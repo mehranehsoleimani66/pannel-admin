1783fbf34f14a460e3a7ff2b5347e1b8d5eabe81
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-11-28 07:53:15 +0330

    add actions.js  like add and delete and update

--- src/app/libs/action.js
+++ src/app/libs/action.js
@@ -1,7 +1,8 @@
 "use server";
+
 import { revalidatePath } from "next/cache";
-import { connectToDB } from "./Utils";
 import { Product, User } from "./models";
+import { connectToDB } from "./utils";
 import { redirect } from "next/navigation";
 import bcrypt from "bcrypt";
 
@@ -18,7 +19,7 @@ export const addUser = async (formData) => {
     const newUser = new User({
       username,
       email,
-      password,
+      password: hashedPassword,
       phone,
       address,
       isAdmin,
@@ -28,7 +29,39 @@ export const addUser = async (formData) => {
     await newUser.save();
   } catch (err) {
     console.log(err);
-    throw new Error(err, "Failed to create user!");
+    throw new Error("Failed to create user!");
+  }
+
+  revalidatePath("/dashboard/users");
+  redirect("/dashboard/users");
+};
+
+export const updateUser = async (formData) => {
+  const { id, username, email, password, phone, address, isAdmin, isActive } =
+    Object.fromEntries(formData);
+
+  try {
+    connectToDB();
+
+    const updateFields = {
+      username,
+      email,
+      password,
+      phone,
+      address,
+      isAdmin,
+      isActive
+    };
+
+    Object.keys(updateFields).forEach(
+      (key) =>
+        (updateFields[key] === "" || undefined) && delete updateFields[key]
+    );
+
+    await User.findByIdAndUpdate(id, updateFields);
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to update user!");
   }
 
   revalidatePath("/dashboard/users");
@@ -61,6 +94,37 @@ export const addProduct = async (formData) => {
   redirect("/dashboard/products");
 };
 
+export const updateProduct = async (formData) => {
+  const { id, title, desc, price, stock, color, size } =
+    Object.fromEntries(formData);
+
+  try {
+    connectToDB();
+
+    const updateFields = {
+      title,
+      desc,
+      price,
+      stock,
+      color,
+      size
+    };
+
+    Object.keys(updateFields).forEach(
+      (key) =>
+        (updateFields[key] === "" || undefined) && delete updateFields[key]
+    );
+
+    await Product.findByIdAndUpdate(id, updateFields);
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to update product!");
+  }
+
+  revalidatePath("/dashboard/products");
+  redirect("/dashboard/products");
+};
+
 export const deleteUser = async (formData) => {
   const { id } = Object.fromEntries(formData);
 
@@ -71,10 +135,9 @@ export const deleteUser = async (formData) => {
     console.log(err);
     throw new Error("Failed to delete user!");
   }
-  revalidatePath("/dashboard/users");
-};
 
-//delete product
+  revalidatePath("/dashboard/products");
+};
 
 export const deleteProduct = async (formData) => {
   const { id } = Object.fromEntries(formData);
@@ -84,7 +147,8 @@ export const deleteProduct = async (formData) => {
     await Product.findByIdAndDelete(id);
   } catch (err) {
     console.log(err);
-    throw new Error("Failed to delet product!");
+    throw new Error("Failed to delete product!");
   }
+
   revalidatePath("/dashboard/products");
 };
