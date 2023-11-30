dd8fcbfc20f00ef99e2784faa8b0ede6f53d66c9
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-11-30 18:15:04 +0330

    added loginform

---
+++ src/app/ui/dashboard/login/loginForm/loginForm.jsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { authenticate } from "@/app/libs/action";
+import { useFormState } from "react-dom";
+
+const LoginForm = () => {
+  const [state, formAction] = useFormState(authenticate, undefined);
+
+  return (
+    <form
+      action={formAction}
+      className="bg-[#182237] p-[50px] rounded-xl w-[500px] h-[500px] flex flex-col gap-[30px] justify-center"
+    >
+      <input
+        name="username"
+        type="text"
+        placeholder="userName..."
+        className="p-[30px] border-2 border-solid border-[#2e374a] bg-[#151c2c] text-white"
+      />
+      <input
+        name="password"
+        type="password"
+        placeholder="password..."
+        className="p-[30px] border-2 border-solid border-[#2e374a] bg-[#151c2c] text-white"
+      />
+      <button className="p-8 bg-teal-500 border-none rounded-md">LogIn</button>
+      {state && state}
+    </form>
+  );
+};
+
+export default LoginForm;
